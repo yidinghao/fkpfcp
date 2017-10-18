fa2b21098291112468598d945f4a896fb513af7c
Yiding Hao <yiding.hao@yale.edu>
2017-10-18 15:23:15 -0400

    Created classes for texts and oracles.

--- oracles.py
+++ oracles.py
@@ -1,28 +1,92 @@
+from abc import ABCMeta, abstractmethod
+
 from nltk import CFG, ChartParser
+from nltk.parse.generate import generate
+
+from scl import Sentence
 
 
-def generates(sentence, grammar=None, parser=None):
+class Text(object):
     """
-    Check whether or not a grammar generates a string.
-
-    :type grammar: CFG
-    :param grammar: The grammar to parse from
-
-    :type sentence: list
-    :param sentence: The sentence to parse
-
-    :param parser: A parser for the grammar (optional)
-
-    :rtype: bool
-    :return: Whether or not grammar generates sentence
+    A text.
     """
-    if grammar is None and parser is None:
-        raise ValueError("A grammar or a parser is required.")
-    if parser is None:
-        parser = ChartParser(grammar)
+    __metaclass__ = ABCMeta
 
-    try:
-        parses = parser.parse(sentence)
-        return list(parses) != []
-    except:
+    @abstractmethod
+    def __init__(self):
+        self._iterator = None
+
+    @abstractmethod
+    def __iter__(self):
+        return self._iterator
+
+
+class GrammarText(Text):
+    """
+    A text from a grammar.
+    """
+
+    def __init__(self, grammar):
+        """
+        Initialize from a CFG.
+
+        :type grammar: CFG
+        :param grammar: A CFG generating the text.
+        """
+        super(GrammarText, self).__init__()
+        self._iterator = generate(grammar)
+
+    def __iter__(self):
+        for i in self._iterator:
+            yield Sentence(i)
+
+
+class Oracle(object):
+    """
+    An oracle.
+    """
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def generates(self, sentence):
+        """
+        A function deciding language membership.
+
+        :type sentence: Sentence
+        :param sentence: A sentence
+
+        :rtype: bool
+        :return: Whether or not the oracle accepts sentence
+        """
         return False
+
+
+class GrammarOracle(Oracle):
+    """
+    An oracle from a grammar.
+    """
+
+    def __init__(self, grammar):
+        """
+        Initialize from a CFG.
+
+        :type grammar: CFG
+        :param grammar: The grammar for this oracle
+        """
+        self._parser = ChartParser(grammar)
+
+    def generates(self, sentence):
+        """
+        Decides whether the grammar generates the sentence.
+
+        :type sentence: Sentence
+        :param sentence: A sentence
+
+        :rtype: bool
+        :return: Whether the grammar generates the sentence
+        """
+        try:
+            parses = self._parser.parse(sentence.get_words())
+            return list(parses) != []
+        except:
+            return False
